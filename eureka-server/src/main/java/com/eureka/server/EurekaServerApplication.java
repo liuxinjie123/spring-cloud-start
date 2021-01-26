4279f076bb2cf1ee6105b462b49d3383e06bfa5a
liuxinjie123 <jackdream@163.com>
2021-01-26 23:33:24 +0800

    delete unused code

--- eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -1,5 +1,9 @@
 package com.eureka.server;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
