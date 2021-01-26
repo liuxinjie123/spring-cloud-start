a3ba9f4cb69e329ea697eb039f4bcfce4a93639b
liuxinjie123 <jackdream@163.com>
2021-01-26 23:09:00 +0800

    delete unused code

---
+++ eureka-consumer/src/main/java/com/eureka/consumer/EurekaConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.eureka.consumer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaConsumerApplication.class).web(true).run(args);
+    }
+}
