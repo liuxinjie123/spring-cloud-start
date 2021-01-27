a599a0faf9671ca46378a79c3490dab7ee217447
liuxinjie123 <jackdream@163.com>
2021-01-27 11:08:42 +0800

    ribbon loanbalancer

--- eureka-consumer-ribbon/src/main/java/com/eureka/ribbon/consumer/EurekaRibbonConsumerApplication.java
+++ eureka-consumer-ribbon/src/main/java/com/eureka/ribbon/consumer/EurekaRibbonConsumerApplication.java
@@ -1,4 +1,22 @@
 package com.eureka.ribbon.consumer;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
 public class EurekaRibbonConsumerApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaRibbonConsumerApplication.class).web(true).run(args);
+    }
 }
